2cd6ac24065fde71e3359b421a3acd5269e62e3b
Wagno <wagnojunior@gmail.com>
2022-12-15 20:31:12 +0900

    after lesson 119 not completed

--- internal/repository/dbrepo/dbrepo.go
+++ internal/repository/dbrepo/dbrepo.go
@@ -12,9 +12,21 @@ type postgresDBRepo struct {
 	DB  *sql.DB
 }
 
+// New type for testing only
+type testDBRepo struct {
+	App *config.AppConfig
+	DB  *sql.DB
+}
+
 func NewPostgresRepo(conn *sql.DB, a *config.AppConfig) repository.DatabaseRepo {
 	return &postgresDBRepo{
 		App: a,
 		DB:  conn,
+	}
+}
+
+func NewTestingPostgresRepo(a *config.AppConfig) repository.DatabaseRepo {
+	return &testDBRepo{
+		App: a,
 	}
 }
